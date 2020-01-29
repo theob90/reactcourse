ea0a8cf4d6d99722caa09283391b08cc20dcd5dd
theob90 <duski.bez@gmail.com>
2020-01-29 07:16:03 +0200

    styling

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
       {name:'Mike', age: 22}
     ],
     //auto de t peirazei
-    otherstate: 'some other state'
+    otherstate: 'some other state',
+    showPersons: false
 
   }
 
@@ -39,16 +40,33 @@ class App extends Component {
     ] })
 
   }
-  render() { 
-    return (
 
-      <div className="App">
-        <h1>
-          Hi I'am a react app
-        </h1>
-        <p>This is really working</p>
-        <button onClick = {() => this.switchNameHandler('theodoros')}>Switching name</button>
-        <Person
+  togglePersonHandler = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+  render() { 
+
+    const style ={
+      backgroundColor:'white',
+      font:'inherit',
+      border: '1px solid blue',
+      padding:'8px',
+      cursor: 'pointer'
+
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div >
+          {this.state.persons.map(person => {
+            return <Person 
+            name={person.name} 
+            age={person.age}/>
+          })}
+        {/* <Person
          name = {this.state.persons[0].name} 
          age ={this.state.persons[0].age}
          click={this.switchNameHandler}/>
@@ -58,7 +76,26 @@ class App extends Component {
           changed={this.nameChangeHandler}
           >My hobbies racing</Person>
         <Person
-         name ={this.state.persons[2].name}age = {this.state.persons[2].age}/>
+         name ={this.state.persons[2].name}age = {this.state.persons[2].age}/> */}
+      </div> 
+
+
+      );
+    }
+
+
+    return (
+
+      <div className="App">
+        <h1>
+          Hi I'am a react app
+        </h1>
+        <p>This is really working</p>
+        <button 
+        style={style}
+        onClick = {this.togglePersonHandler}>Switching name</button>
+        {persons}
+        {/* anaferetai st ket persons */}
       </div>
 
       );
--- myapp/src/Person/Person.js
+++ myapp/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
     
-    <div>
+    <div className="Person">
         <p onClick={props.click}>I am a {props.name} and i am {props.age} years old</p>
         <p>{props.children}</p>
         <input type ="text"onChange={props.changed}
