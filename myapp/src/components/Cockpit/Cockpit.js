1195e94a2916811eb0a29e6a38b344b1b8d456a1
theob90 <duski.bez@gmail.com>
2020-02-10 07:15:29 +0200

    memo

--- myapp/src/components/Cockpit/Cockpit.js
+++ myapp/src/components/Cockpit/Cockpit.js
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
 import classes from './Cockpit.css';
 
-const cockpit = (props) =>{
+const cockpit = ( props ) => {
+  //ekteleite se kathe render
+  useEffect(() =>{
+    console.log('Cockpit.js useEffect');
+
+    setTimeout(() =>{
+      alert('Saved');
+    }, 1000);
+   
+   // ginetai otan allazei t persons...einai parametros 
+  // an thelw na treksei kathe fora pou allazi to person
+  // tha vale  [props.persons]  
+  //me t keno tha treksei mia fora
+  return () =>{
+
+    console.log('[Cockpit.js] cleanup in useEffect' );
+  };
+  }, []);
+
+
 
     const assignedClasses = [];
     let btnClass = '';
-    if(props.showPersons){
+    if (props.showPersons) {
         btnClass = classes.Red;
-
     }
 
-    if (props.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
+    if ( props.personsLenght <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
-    if (props.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+    if ( props.perpersonsLenghtsons <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
 
-    return(
+    return (
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
+            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
             <button
-                className={btnClass} onClick={props.clicked}>
-                Toggle Persons
-            </button>
-        
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
         </div>
-
     );
-
 };
-export default cockpit;
+
+export default React.memo(cockpit);
+// kanei render otan exoume allagi st input
--- myapp/src/containers/App.js
+++ myapp/src/containers/App.js
@@ -5,13 +5,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
 
-constructor(props){
-  super(props);
-  console.log('App.js constructor');
-}
-
-//mporw na t valw st contructor alla me this.state
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -19,12 +17,30 @@ constructor(props){
       { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   };
 
-  static getDerivedStateFromProps(props,state){
-    console.log('App.js getDerivedStateFromProps ', props);
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
     return state;
+  }
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
@@ -59,27 +75,34 @@ constructor(props){
   };
 
   render() {
-    console.log('App.js render')
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
-      persons =<Persons  
-          persons = {this.state.persons}
+      persons = (
+        <Persons
+          persons={this.state.persons}
           clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler}/>;
-      
-
+          changed={this.nameChangedHandler}
+        />
+      );
     }
-
-
 
     return (
       <div className={classes.App}>
-        <Cockpit 
-        title={this.props.appTitle}
-        showPersons={this.state.showPersons}
-        persons={this.state.persons}
-        clicked={this.togglePersonsHandler}  />
+        <button onClick={()=>{
+          this.setState({ showCockpit: false});
+        }}
+        
+        >Remove Cockpit</button>
+        {this.state.showCockpit ? (
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          personsLength={this.state.persons.length}
+          clicked={this.togglePersonsHandler}
+        />
+        ) : null}
         {persons}
       </div>
     );
@@ -88,4 +111,3 @@ constructor(props){
 }
 
 export default App;
-////
